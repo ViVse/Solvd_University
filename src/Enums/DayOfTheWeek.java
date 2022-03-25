9a7bdad7987c1d1fa73714b7dc971d74862f709a
ViVse <ivanisenko730@gmail.com>
2022-03-25 16:35:34 +0200

    updated DayOfTheWeek

--- src/Enums/DayOfTheWeek.java
+++ src/Enums/DayOfTheWeek.java
@@ -2,7 +2,7 @@ package Enums;
 
 public enum DayOfTheWeek{
     MON("Monday"), TUE("Tuesday"), WED("Wednesday"), THU("Thursday"), FRI("Friday"), SAT("Saturday"), SUN("Sunday");
-    private String value;
+    private final String value;
     DayOfTheWeek(String value){
         this.value = value;
     }
